a1fbf08c5ad3a42707a80e91c7bdbd7807eed097
pitt500 <pedro.rojaslo50@gmail.com>
2020-01-21 17:46:20 -0600

    Starter Project

---
+++ SwiftUI101/SwiftUI101/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI101
+//
+//  Created by projas on 1/21/20.
+//  Copyright © 2020 pedrorojas.dev. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
